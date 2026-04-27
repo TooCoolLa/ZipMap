df0e12d53af4d764ca3368ecf7b2a4b194e42c3e
Gao YuCheng <gaoyucheng@ustc.edu.com>
2026-04-27 11:59:27 +0800

    feat: implement ImageLoaderWorker and ResultSaverWorker

--- zipmap/utils/streaming_utils.py
+++ zipmap/utils/streaming_utils.py
@@ -1,6 +1,10 @@
 import threading
 import queue
 import abc
+import numpy as np
+import os
+import torch
+from zipmap.utils.load_fn import load_and_preprocess_images
 
 class BaseWorker(threading.Thread, abc.ABC):
     def __init__(self, in_queue, out_queue=None, name=None):
@@ -35,3 +39,22 @@ class BaseWorker(threading.Thread, abc.ABC):
     def stop(self):
         self.stop_event.set()
         self.join()
+
+class ImageLoaderWorker(BaseWorker):
+    def process(self, image_path):
+        # 预处理图片
+        # 注意: load_and_preprocess_images 接受一个列表，返回 (N, 3, H, W) 的 tensor
+        tensor = load_and_preprocess_images([image_path])
+        return {"path": image_path, "tensor": tensor}
+
+class ResultSaverWorker(BaseWorker):
+    def process(self, data):
+        predictions = data["predictions"]
+        save_path = data["save_path"]
+        
+        # 确保保存目录存在
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        
+        # 使用 np.savez_compressed 以节省磁盘空间
+        np.savez_compressed(save_path, **predictions)
+        return None
